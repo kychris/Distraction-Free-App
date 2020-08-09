ab98fb3173cce7f834556f76c81f9a688aebafcf
kychris <kychris1999@hotmail.com>
2020-08-09 09:56:38 -0700

    restructure entire program
    through utilizing 5 helper functions, successfully reduced entire program size  by around half, all common functions are now encapsulized, and extremly easy to understand

--- DistractionFree App/ViewController/ViewController.swift
+++ DistractionFree App/ViewController/ViewController.swift
@@ -29,165 +29,63 @@ class ViewController: UIViewController {
             tmpButton?.isOn = UserDefaults.standard.bool(forKey: curRuleName)
         }
         
+        //reset system
         resetCache()
         detectContentBlockerEnabling()
+        refreshBlocker()
         
-        //refresh reloader
-        SFContentBlockerManager.reloadContentBlocker(withIdentifier: "com.Christian.DistractionFree-App.DistractionBlock", completionHandler: { error in
-            print(error)
-        })
     }
     
     @IBAction func AddRule(_ sender: UISwitch) {
         
         let toggleName: String = ruleNames[sender.tag-1]
         
-        let decoder = JSONDecoder()
-        let encoder = JSONEncoder()
-        
-        
-        guard let taskJSONURL = Bundle.main.url(forResource: "rules", withExtension: "json") else {
-            return
-        }
-        
         if(sender.isOn) { //if toggle is enabled
-            UserDefaults.standard.set(true, forKey: toggleName)
-            do {
-                let groupUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.distract")
-//                print(groupUrl)
-                let exportJsonURL = URL(fileURLWithPath: "export", relativeTo: groupUrl).appendingPathExtension("json")
-                let exports = try Data(contentsOf: exportJsonURL)
-                var exportList = try decoder.decode([Rule].self, from: exports)
-                
-                let rules = try Data(contentsOf: taskJSONURL)
-                let ruleList = try decoder.decode([Rule].self, from: rules)
-                for r in ruleList {
-                    if r.name == toggleName {
-                        exportList.append(r)
-                    }
-                }
-                
-                print(exportList)
-                
-                let exportData = try encoder.encode(exportList)
-                try exportData.write(to: exportJsonURL)
-                
-            } catch let error {
-                print(error)
-            }
+            
+            UserDefaults.standard.set(true, forKey: toggleName) //save setting to permanent storage
+            
+            //read, add rule, then export
+            var exportList = readCurrentExport()
+            let ruleList = getRules()
+            exportList.append(findRule(ruleList: ruleList, toggleName: toggleName))
+            writeToOutput(exportList: exportList)
+            print(exportList)
             
         } else { //if sender is disabled
-            UserDefaults.standard.set(false, forKey: toggleName)
-            do {
-
-                let groupUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.distract")
-                let exportJsonURL = URL(fileURLWithPath: "export", relativeTo: groupUrl).appendingPathExtension("json")
-                var exports = try Data(contentsOf: exportJsonURL)
-                var exportList = try decoder.decode([Rule].self, from: exports)
-
-                for i in 0..<exportList.count {
-                    if exportList[i].name == toggleName {
-                        if(exportList.count > 1) {
-                            exportList.remove(at: i)
-                            break
-                        } else {
-                            let rules = try Data(contentsOf: taskJSONURL)
-                            let ruleList = try decoder.decode([Rule].self, from: rules)
-                            exportList.append(ruleList[0])
-                            exportList.remove(at: 0)
-                        }
+            
+            UserDefaults.standard.set(false, forKey: toggleName) //save setting to permanent storage
+            
+            //remove current rule from output
+            var exportList = readCurrentExport()
+            for i in 0..<exportList.count {
+                if exportList[i].name == toggleName {
+                    if(exportList.count > 1) { //if export not empty, remove item
+                        exportList.remove(at: i)
+                        break
+                    } else { //if export is empty, append placeholder
+                        let ruleList = getRules()
+                        exportList.append(ruleList[0])
+                        exportList.remove(at: 0)
                     }
                 }
-   
-                print(exportList)
-                
-                let exportData = try encoder.encode(exportList)
-                try exportData.write(to: exportJsonURL)
-            } catch let error {
-                print(error)
             }
+            writeToOutput(exportList: exportList)
+            print(exportList)
         }
-        
-        //refreshed safari content blocker
-        SFContentBlockerManager.reloadContentBlocker(withIdentifier: "com.Christian.DistractionFree-App.DistractionBlock", completionHandler: { error in
-                    print(error)
-        })
-    }
-
-    @IBAction func Toggle(_ sender: UISwitch) {
-        
-        //read json file in bundle
-        guard let taskJSONURL = Bundle.main.url(forResource: "rules", withExtension: "json") else {
-            return
-        }
-          
-        let decoder = JSONDecoder()
-
-        do {
-            let taskData = try Data(contentsOf: taskJSONURL)
-    
-            let task = try decoder.decode([Rule].self, from: taskData)
-            for t in task {
-                print("name")
-                print(t.name)
-                print ("action")
-                print (t.action)
-                print ("trigger")
-                print (t.trigger)
-                print ("================")
-            }
-            
-        } catch let error {
-            print(error)
-        }
-        
-    
-        if (sender.isOn) {
-            
-        } else {
-            
-        }
+        refreshBlocker() //refreshed safari content blocker
     }
     
-    //reset save json file
     func resetCache() {
-        //start coders
-        let decoder = JSONDecoder()
-        let encoder = JSONEncoder()
-        do {
-            var exportList: [Rule] = [] //final export
-            //read rule list json
-            guard let taskJSONURL = Bundle.main.url(forResource: "rules", withExtension: "json") else {
-                return
+        var exportList: [Rule] = []
+        let ruleList = getRules()
+        exportList.append(ruleList[0])
+        for rule in ruleList {
+            if (UserDefaults.standard.bool(forKey: rule.name)) {
+                exportList.append(rule)
             }
-            let rules = try Data(contentsOf: taskJSONURL)
-            let ruleList = try decoder.decode([Rule].self, from: rules)
-            
-            //append default and rules that are true
-            exportList.append(ruleList[0])
-            for rule in ruleList {
-                if (UserDefaults.standard.bool(forKey: rule.name)) {
-                    exportList.append(rule)
-                }
-            }
-            
-            //export new selected rules to group
-            let groupUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.distract")
-            let exportJsonURL = URL(fileURLWithPath: "export", relativeTo: groupUrl).appendingPathExtension("json")
-            let exportData = try encoder.encode(exportList)
-            try exportData.write(to: exportJsonURL)
-            
-            
-            
-            //refresh blocker
-            SFContentBlockerManager.reloadContentBlocker(withIdentifier: "com.Christian.DistractionFree-App.DistractionBlock", completionHandler: { error in
-                print(error)
-            })
-            
-        } catch let error {
-            print("Me created: Reset Cache ERROR")
-            print(error)
         }
+        writeToOutput(exportList: exportList)
+        refreshBlocker()
     }
     
     func detectContentBlockerEnabling() {
@@ -204,6 +102,7 @@ class ViewController: UIViewController {
             }
         })
     }
+    
     @IBAction func testButton(_ sender: UIButton) {
         resetCache()
     }
@@ -225,6 +124,7 @@ class ViewController: UIViewController {
             return []
         }
     }
+    
     func writeToOutput(exportList: [Rule]) {
         let encoder = JSONEncoder()
         do {
